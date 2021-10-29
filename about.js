db03c4465633a51ce6b877c3e139ece8fb581fc6
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-29 18:20:01 -0500

    changed image to Bright Path's image - Added welcome paragraph - added bulleted list with 3 compliments - added ^how did you hear about us^ form - added alert on submit button - added event listener and alert to image

--- about.js
+++ about.js
@@ -5,10 +5,16 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Your form has been successfully submit');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+////////////////////
+
+function mouseOver(){
+	alert('Actions speak louder than words, and yours tell an incredible story.');
+}
