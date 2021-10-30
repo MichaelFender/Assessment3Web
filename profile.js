72428520b7bfcb64062200d6aaf16f627ecea829
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-29 19:51:32 -0500

    added my FAV buttons and added event listeners to profige

---
+++ profile.js
@@ -0,0 +1,11 @@
+document.getElementById("color").addEventListener("click", function() {
+    document.getElementById("demo").innerHTML = "My Favorite color is BLUE";
+  });
+
+  document.getElementById("place").addEventListener("click", function() {
+    document.getElementById("demo").innerHTML = "My favorite place is Broken Bow, Oklahoma";
+  });
+
+  document.getElementById("ritual").addEventListener("click", function() {
+    document.getElementById("demo").innerHTML = "My favorite ritual is morning coffee with my wife :)";
+  });
