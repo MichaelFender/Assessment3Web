7dff9a652075f5be21ab3cfb71b08c56a4d747f8
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-29 19:35:42 -0500

    added content to profile page and created profile.js

--- about.js
+++ about.js
@@ -8,6 +8,12 @@ function handleSubmit(evt) {
 	alert('Your form has been successfully submit');
 }
 
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('Your form has been successfully submit');
+}
+
 
 let form = document.querySelector('form#contact');
 
@@ -16,5 +22,9 @@ form.addEventListener('submit', handleSubmit);
 ////////////////////
 
 function mouseOver(){
+	alert('Actions speak louder than words, and yours tell an incredible story.');
+}
+
+function compliment(){
 	alert('Actions speak louder than words, and yours tell an incredible story.');
 }
