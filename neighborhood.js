907bf7bb40bc9d3345f4e0bb525dea334e22fdb8
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-31 20:37:09 -0500

    added randomizer button for local resturaunts to NEIGHBORHOOD.js

--- neighborhood.js
+++ neighborhood.js
@@ -1,5 +1,27 @@
-const resturaunts = ["Prejeans Cajun Resturaunt", "Billys Boudin", "Buffalo Wild Wings", "Pouparts Bakery", "Deanos Pizza", "Zea's Grill", "Tsunami"];
-
-let random = Math.floor(Math.random() * resturaunts.length)
-
-form.addEventListener('submit', handleSubmit);
+var findRandomRestaurant = {
+    restaurant: "",
+    findRestaurant: function() {
+      var fillMyBelly = Math.random();
+      if (fillMyBelly < 0.20) {
+        this.restaurant = "Prejeans Cajun Restaurant";
+      } else if (fillMyBelly < 0.40) {
+        this.restaurant = "Billy's Boudin";
+      } else if (fillMyBelly < 0.60) {
+        this.restaurant = "Meche's Doughnuts";
+      } else if (fillMyBelly < 0.80) {
+        this.restaurant = "Tsunami Sushi";
+      } else if (fillMyBelly < 1) {
+        this.restaurant = "Central Pizza";
+      } else {
+        this.restaurant = "Spoonbill";
+      }
+    }
+  };
+  
+  var myRestaurantButton = document.getElementById('restaurant');
+  myRestaurantButton.addEventListener('click', function() {
+    
+    findRandomRestaurant.findRestaurant();
+    document.getElementById("restaurantButton").innerHTML = findRandomRestaurant.restaurant;
+  
+});
